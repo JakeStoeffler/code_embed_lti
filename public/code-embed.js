af9f5732ce66264fd5b74282dc8234a1cc9f7d05
Jake Stoeffler <jake@jakestoeffler.com>
2013-06-10 06:04:56 -0400

    Fixed security issue by doing escape/unescape

--- public/code-embed.js
+++ public/code-embed.js
@@ -1,22 +1,25 @@
 function setupEditor() {
-	// encode the editor's contents in case it contains any HTML code
-	//encodeContents(editorEl);
-	var editor = ace.edit("editor");
-	var editorSess = editor.getSession();
-	editor.setTheme("ace/theme/monokai");
-	editorSess.setMode("ace/mode/javascript");
-	editorSess.setUseWrapMode(true);
+  // unescape the editor's contents before creating ACE
+  var $editorEl = $('#editor');
+  $editorEl.text(unescape($editorEl.text()));
+  // create the ACE editor
+  var editor = ace.edit("editor");
+  var editorSess = editor.getSession();
+  // change settings
+  editor.setTheme("ace/theme/monokai");
+  editorSess.setMode("ace/mode/javascript");
+  editorSess.setUseWrapMode(true);
   
-	var editorDiv = document.getElementById("editor");
-	editorDiv.style.display = "block";
-    $(editorDiv).resizable();
-    return editor;
+  // we don't display the editor until now to prevent flickering on page load
+  $editorEl.show();
+  $editorEl.resizable(); // make editor resizable with handles using jQuery UI
+  return editor;
 }
 
 // eventually this will be taken care of on server side
-function encodeContents(element) {
-   var div = document.createElement('div');
-   var text = document.createTextNode(element.innerHTML);
-   div.appendChild(text);
-   element.innerHTML = div.innerHTML;
+function getEncodedContents(element) {
+  var div = document.createElement('div');
+  var text = document.createTextNode(element.innerHTML);
+  div.appendChild(text);
+  return div.innerHTML;
 }
