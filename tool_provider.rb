0c6c2c4b499c8b90ee2484e6a3e3bcf53da9316e
Jake Stoeffler <stoefflerj@gmail.com>
2014-01-25 16:52:55 -0600

    Remove tool_consumer_instance_guid parameter requirement

--- tool_provider.rb
+++ tool_provider.rb
@@ -68,9 +68,9 @@ end
 # It will verify the OAuth signature
 post '/lti_tool' do
   authorize!
-  return "missing required values: #{params}" unless params['resource_link_id'] && params['tool_consumer_instance_guid']
+  return "missing resource_link_id in request: #{params}" unless params['resource_link_id']
   placement_id = params['resource_link_id'] + 
-      params['tool_consumer_instance_guid']
+      (params['tool_consumer_instance_guid'] or "")
   placement = Placement.first(:placement_id => placement_id)
   # If placement already exists, set up and display an editor with stored =
   # contents and settings; else, let user create new editor placement.
