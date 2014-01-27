589a777af18698ac5c911afc81cc71b33e1578ad
Jake Stoeffler <stoefflerj@gmail.com>
2014-01-27 00:25:09 -0600

    Make our own random placement_id as a workaround to Canvas' non-unique resource_link_ids

--- tool_provider.rb
+++ tool_provider.rb
@@ -92,7 +92,18 @@ post '/lti_tool' do
   logger.info "POST /lti_tool"
   return erb :error unless authorize!
   return "missing resource_link_id in request: #{params}" unless params['resource_link_id']
-  placement_id = params['resource_link_id'] + (params['tool_consumer_instance_guid'] or "")
+  
+  # If this if for a rich content editor, set up the return url
+  if @tp.is_content_for?(:embed) && @tp.accepts_iframe?
+    # make a random placement_id since Canvas doesn't give us unique ids with the editor button launch
+    placement_id = (0...20).map { ((0..9).to_a+('a'..'z').to_a+('A'..'Z').to_a)[rand(62)] }.join
+    url = "https" + "://" + request.host_with_port + "/placement/" + placement_id
+    return_url = @tp.iframe_content_return_url(url, 600, 400, "Code Embed")
+  else
+    return_url = nil
+    placement_id = params['resource_link_id'] + (params['tool_consumer_instance_guid'] or "")
+  end
+  
   logger.info "placement_id: #{placement_id}"
   placement = Placement.first(:placement_id => placement_id)
   # If placement already exists, set up and display an editor with stored =
@@ -113,12 +124,6 @@ post '/lti_tool' do
   end
   @tp.lti_msg = "Thanks for using Code Embed!"
   
-  return_url = nil
-  # If this if for a rich content editor, set up the return url
-  if @tp.is_content_for?(:embed) && @tp.accepts_iframe?
-    url = "https" + "://" + request.host_with_port + "/placement/" + placement_id
-    return_url = @tp.iframe_content_return_url(url, 600, 400, "Code Embed")
-  end
   # code_embed will set things up accordingly
   erb :code_embed, :locals => { :content => content,
                                 :editor_settings => editor_settings,
