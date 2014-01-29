e4da0b6392c92f4122bdda97f7513d52f06e55bb
Jake Stoeffler <stoefflerj@gmail.com>
2014-01-28 22:30:14 -0600

    Make old placements work

--- tool_provider.rb
+++ tool_provider.rb
@@ -93,27 +93,29 @@ post '/lti_tool' do
   return erb :error unless authorize!
   return "missing resource_link_id in request: #{params}" unless params['resource_link_id']
   
-  # Set up the return url
-  url = "https" + "://" + request.host_with_port + "/placement/" + placement_id
-  #if @tp.is_content_for?(:embed) && @tp.accepts_iframe?
-  if @tp.accepts_iframe?
-    # make a random placement_id since Canvas doesn't give us unique ids with the editor button launch
-    placement_id = (0...20).map { ((0..9).to_a+('a'..'z').to_a+('A'..'Z').to_a)[rand(62)] }.join
-    return_url = @tp.iframe_content_return_url(url, 600, 400, "Code Embed")
-  else
-    placement_id = params['resource_link_id'] + (params['tool_consumer_instance_guid'] or "")
-    return_url = url
-  end
-  logger.info "placement_id: #{placement_id}"
+  old_placement_id = params['resource_link_id'] + (params['tool_consumer_instance_guid'] or "")
   
   placement = Placement.first(:placement_id => placement_id)
   # If placement already exists, set up and display an editor with stored =
   # contents and settings; else, let user create new editor placement.
   if placement
+    logger.info "existing placement: #{placement_id}"
     content = placement.content
     editor_settings = placement.editor_settings
     hide_settings = true
   else
+    logger.info "new placement: #{placement_id}"
+    # Set up the return url
+    url = "https" + "://" + request.host_with_port + "/placement/" + placement_id
+    #if @tp.is_content_for?(:embed) && @tp.accepts_iframe?
+    if @tp.accepts_iframe?
+      # make a random placement_id since Canvas doesn't give us unique ids with the editor button launch
+      placement_id = (0...20).map { ((0..9).to_a+('a'..'z').to_a+('A'..'Z').to_a)[rand(62)] }.join
+      return_url = @tp.iframe_content_return_url(url, 600, 400, "Code Embed")
+    else
+      placement_id = old_placement_id
+      return_url = url
+    end
     content = "// Welcome to Code Embed!\n" + # default content
       "// To get started, select the language you want to code in and pick a theme!\n" +
       "// Feel free to play around with the other settings as well.\n" +
@@ -123,6 +125,7 @@ post '/lti_tool' do
     # use a cookie-based session to remember placement permission
     flash["can_save_" + placement_id] = true
   end
+  
   @tp.lti_msg = "Thanks for using Code Embed!"
   
   # code_embed will set things up accordingly
